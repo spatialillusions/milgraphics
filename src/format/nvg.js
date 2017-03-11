4c301e7219c1c4d4a8b2a8de0e3ac6ed53abbccf
Måns Beckman <mans.beckman@spatialillusions.com>
2017-03-11 10:07:04 +0100

    Add class for NVG

--- src/format/nvg.js
+++ src/format/nvg.js
@@ -1,21 +1,6 @@
 
-function GeoJSON(data, mapping) {
-  //if (typeof mapping === 'undefined') return data;
-  var feature_copy = [];
-  for (var i = 0; i < data.features.length; i++) {
-    feature = data.features[i];
-    var f = {type: "Feature", properties: {} }
-    f.geometry = {type: feature.geometry.type, coordinates: feature.geometry.coordinates };
-    for (key in feature.properties) {
-      if (mapping.hasOwnProperty(key)){
-        f.properties[mapping[key]] = feature.properties[key];
-      }else{
-        f.properties[key] = feature.properties[key];
-      }
-    }
-    feature_copy.push(f);
-  }
-	return {type: "FeatureCollection", features: feature_copy };
+function NVG(data) {
+//TODO
 }
 
-module.exports = GeoJSON;
+module.exports = NVG;
