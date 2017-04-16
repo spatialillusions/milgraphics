bc6a760e8e914c23f8d28e4e3360ee19f42cae36
Måns Beckman <mans.beckman@spatialillusions.com>
2017-04-16 17:27:09 +0200

    Add .eslintrc

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    "browser": true,
+    "node": true,
+    "commonjs": true
+  },
+  extends: "eslint:recommended",
+  rules: {
+    "linebreak-style": ["error", "unix"],
+    "no-console": ["error", { allow: ["info","warn", "error"] }],
+    //quotes: ["error", "double"],
+    "no-unused-vars": ["error", { "vars": "all", "args": "none"}],
+    semi: ["error", "always"]
+  }
+};
