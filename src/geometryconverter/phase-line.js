7f1061bed9dbab3550fd517442a9e1c927aea9b9
Martin Kučera <mkucera@retia.cz>
2021-05-10 15:42:56 +0200

    refactoring phase-line, update disk/milgraphics.js

--- src/geometryconverter/phase-line.js
+++ src/geometryconverter/phase-line.js
@@ -1,50 +1,46 @@
 var ms = require("milsymbol");
-var annotations = [{},{},{}];
-
 
 function phaseLine(feature) {
   //var direction, width;
   var points = feature.geometry.coordinates;
-
-  var geometry = {type: "MultiLineString"};
-  geometry.coordinates = [];
+  var annotations = [];
+  var geometry = {
+    type: "MultiLineString"
+  };
   var geometry1 = [];
-
   for (var i = 1; i < points.length; i += 1) {
     // measure distance between each two points
     distance = ms.geometry.distanceBetween(points[i - 1], points[i])
 
     // Making each segment straight
     geometry1 = laundery(geometry1, points[i - 1], points[i], 0, 0)
-
   }
   geometry.coordinates = [geometry1];
     
-addAnotation(0,points[0],"(PL NAME)");
-addAnotation(1,points[4],"(SOMETHING)");
-addAnotation(2,points[7],"(PL NAME)");
+  annotations.push(addAnotation(points[0],"(PL NAME)"));
+  annotations.push(addAnotation(points[4],"(SOMETHING)"));
+  annotations.push(addAnotation(points[7],"(PL NAME)"));
 
   return {geometry: geometry, annotations: annotations};
 }
 
 function laundery(geo, pointa, pointb, degree = 0) {
-
   if (degree <= 0) {
     geo.push(pointa, pointb)
     return geo;
   }
-
-  const width = ms.geometry.distanceBetween(pointa, pointb);
-  const midpoint = ms.geometry.pointBetween(pointa, pointb, 0.5);
-
 }
 
-function addAnotation(annotationNumber, annotationPoint, annotationText){
-  
-  annotations[annotationNumber].geometry = {type: "Point"};
-  annotations[annotationNumber].properties = {};
-  annotations[annotationNumber].geometry.coordinates = annotationPoint;
-  annotations[annotationNumber].properties.text = annotationText;
+function addAnotation(annotationPoint, annotationText){
+  return ({
+    geometry: {
+      type: "Point",
+      coordinates: annotationPoint
+    },
+    properties: {
+      text: annotationText
+    }
+  });
 }
 module.exports = phaseLine;
 
