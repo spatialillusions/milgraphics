a53ed233b274755d45bb247a2892aa1a477a02ca
Martin Kučera <mkucera@retia.cz>
2021-05-12 20:43:47 +0200

    refactor boundaries.js function

--- src/geometryconverter/boundaries.js
+++ src/geometryconverter/boundaries.js
@@ -1,31 +1,28 @@
 var ms = require("milsymbol");
-//Converting lines to dashed lines
-const convertToDashes = require("../geometry/converttodashes");
 
 function boundaries(feature) {
-  //var direction, width;
-  var annotations = [];
   var points = feature.geometry.coordinates;
   var geometry = { type: "MultiLineString", coordinates: [] };
 
-  var geometry1 = [];
-  for (var i = 0; i < points.length; i += 1) {
-    // Making each segment straight
-    geometry1 = flotify(geometry1, points[i], points[i], 0, 0)
-    annotations.push(ms.geometry.addAnotation(points[i], "III"));
+  var point = [];
+  if (points.length % 2 === 0) {
+    var index = points.length / 2;
+    var f = ms.geometry.distanceBetween(points[index - 1], points[index]);
+    point = ms.geometry.pointBetweenAbsolute(points[index - 1], points[index], f / 2);
+  } else {
+    point = points.slice((points.length - 1) / 2)[0];
   }
-  //Converting lines to dashed lines
-  geometry.coordinates = convertToDashes(geometry1, 1 / 64);
   
-  return { geometry: geometry, annotations: annotations };
+  var annotations = {
+    geometry: { type: "Point", coordinates: point },
+    properties: { text: "III" }
+  };
+
+  geometry.coordinates = [points];
+  
+  return { geometry: geometry, annotations: [annotations] };
 }
 
-function flotify(geo, pointa, pointb, degree = 0) {
-  if (degree <= 0) {
-    geo.push(pointa, pointb)
-    return geo;
-  }
-}
 
 module.exports = boundaries;
 
