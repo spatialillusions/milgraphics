4a0ebf9e16365468e05fc557a19e6034860b2062
ondrej-podolsky <on.podolsky@gmail.com>
2021-11-24 20:55:26 +0100

    Update

--- src/geometry/circlecorridorpolygon.js
+++ src/geometry/circlecorridorpolygon.js
@@ -13,12 +13,12 @@ module.exports = function(feature) {
             break;
         case "LineString":
             geometry = ms.geometry.rectangle(feature).geometry;
-            //annotation.geometry.coordinates = feature.geometry.coordinates;
+            /*annotation.geometry.coordinates = feature.geometry.coordinates;
             annotation.geometry.coordinates = ms.geometry.pointBetween(
                 feature.geometry.coordinates[0],
                 feature.geometry.coordinates[1],
                 0.5
-            );
+            );*/
             //console.log(annotation);
             break;
         case "Polygon":
--- src/geometryconverter/air-corridor.js
+++ src/geometryconverter/air-corridor.js
@@ -3,20 +3,23 @@ const toDistanceBearing = require("../geometry/todistancebearing");
 
 module.exports = function (feature) {
   var annotations = [];
-  var points = feature.geometry.coordinates;
   var annotationText = feature.properties.name;
+  var points = feature.geometry.coordinates;
   var distance = feature.properties.distance; //distance in meters
-
   var centerPoint = points;
   var topP = ms.geometry.toDistanceBearing(centerPoint, distance, 360);
   var bottP = ms.geometry.toDistanceBearing(centerPoint, distance, 180);
-   
-  annotations.push(ms.geometry.addAnotation(centerPoint[0], "1"));
-  annotations.push(ms.geometry.addAnotation(centerPoint[1], "2"));
-
-  console.log(topP, bottP);  
+  var sym = new ms.Symbol("SFG-UCI----D");
+  sym.setOptions({size:35});
+  var canvasElement = sym.asCanvas();
+  
+  annotations.push(ms.geometry.addAnotation(centerPoint[0], "ACP 1"));
+  annotations.push(ms.geometry.addAnotation(centerPoint[1], "CCP 1"));
+  annotations.push(ms.geometry.addAnotation(centerPoint[2], "ACP 2"));
+  
   var shape = ms.geometry.corridor(feature);
-
+  
+  
   return {
     geometry: shape.geometry,
     annotations: annotations
